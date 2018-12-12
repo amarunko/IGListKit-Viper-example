f6fa9f20b8a3cc136cd7e745bd349b55e1e0f974
Anton Marunko <anton.marunko@btsdigital.kz>
2018-12-12 21:31:32 +0600

    Initial Commit

---
+++ IGListKit-Viper-exampleTests/IGListKit_Viper_exampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  IGListKit_Viper_exampleTests.swift
+//  IGListKit-Viper-exampleTests
+//
+//  Created by Anton Marunko on 12/12/2018.
+//  Copyright © 2018 Hone. All rights reserved.
+//
+
+import XCTest
+@testable import IGListKit_Viper_example
+
+class IGListKit_Viper_exampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
